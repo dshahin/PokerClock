152fa0deaa28a2d79c28d1c3e29b6bcfa3f06e7d
Dan Shahin <dan@codescience.com>
2014-08-19 13:14:10 -0700

    tweaked voice settings and removed cruft

--- js/pokerClock.js
+++ js/pokerClock.js
@@ -2,13 +2,13 @@ $(function () { pokerClock.init(); });
 
 var pokerClock = {
 	voiceOptions : {
-		voiceName : 'Alex',
+		voiceName : 'Google UK English Male',
 		enqueue: true,
 		pitch : 0.42,
-		rate : 1.67,
+		rate : 0.78,
 		volume: 1
 	},
-	voiceNames :['Zardoz','Agnes'],
+	voiceNames :['Alex'],
 	say : function(quote){
 		if(!pokerClock.mute){
 			chrome.tts.speak(quote, pokerClock.voiceOptions);
@@ -27,7 +27,7 @@ var pokerClock = {
               }else{
               	$('#voiceName').append('<option value="' + vName + '">' + vName + '</option>');
               }
-              
+
             }
           });
 
@@ -101,9 +101,7 @@ var pokerClock = {
 		});
 		$("#pauseButton").toggle(pokerClock.pauseCountdown, pokerClock.startCountdown).click();
 		$("#soundButton").toggle(pokerClock.muteOn,pokerClock.muteOff).css({color:'green'});
-		// $("#addRound").bind('click', pokerClock.addRound);
-		// $("#addPlayer").bind('click', pokerClock.addPlayer);
-		// $("#randomizeSeats").bind('click', pokerClock.randomizeSeats);
+
 		$("#startRound").bind('click', function(){ pokerClock.startRound(pokerClock.currentRound) })
 						.attr({title:'restart current round'});
 		$(".nextRound").attr({title:'next round'})
@@ -138,18 +136,13 @@ var pokerClock = {
 		$("#structure").change(function(){
 			pokerClock.loadStructure($(this).val());
 		});
-		$("#payStructure").change(function(){
-			pokerClock.loadPayStructure($(this).val());
-		});
+
 
 		$("#rounds input[type='text']").live('change', pokerClock.updateRounds);
-		$("#players input[type='text']").live('change', pokerClock.updatePlayers);
 		pokerClock.showStructures();
 
 		pokerClock.loadStructure(0);
-		pokerClock.loadPayStructure(0);
 
-		$("#payouts input.poPercent").live('change', pokerClock.calculatePayoutDollars).change();
 
 		$( "#tabs" ).tabs();
 
@@ -344,7 +337,7 @@ var pokerClock = {
 
 			$("#rounds").append(
 				'<tr class="rounds">'+
-				
+
 				'<td class="rounds small"><input disabled="disabled" type="text" class="text small" value="' + pokerClock.rounds[r].small + '"/></td>' +
 				'<td class="rounds big"><input disabled="disabled" type="text" class="text big" value="' + pokerClock.rounds[r].big + '"/></td>' +
 				'<td class="rounds"><input type="text" disabled="disabled" class="text ante" value="' + pokerClock.rounds[r].ante + '"/></td>' +
@@ -447,94 +440,7 @@ var pokerClock = {
 	},
 	rounds : [ ],
 	defaultRound : {minutes: 20, small: 25, big: 50, ante: 0},
-	defaultPlayer : {name: 'new player', buyIn: 20, chips: 2000, seat: 0, ko: false},
-	players : [ ],
-	addPlayer : function(){
-		var num = parseInt($(".numPlayers").val());
-		var buyin =$("#buyin").val();
-		var chips =$("#chips").val();
-		for(var i = 0; i < num; i++){
-			//var p = jQuery.extend({}, pokerClock.defaultPlayer);
-			var p = {'buyIn':buyin, 'chips':chips};
-			p.name = 'player ' + (pokerClock.players.length + 1);
-			pokerClock.players.push(p);
-		}
-		pokerClock.randomizeSeats();
 
-	},
-	showPlayers : function(){
-		$("#players tbody").remove();
-		var totalBuyIn = 0;
-		var totalChips = 0;
-		var numPlayers = pokerClock.players.length;
-		var rowString;
-		for(var i in pokerClock.players){
-			if (pokerClock.players[i].ko === true){
-				rowString = '<tr class="player ko ">';
-			}else{
-				rowString = '<tr class="player">';
-			}
-			var rowString =
-					rowString +
-					'<td class="player seat">'+pokerClock.players[i].seat +'</td>' +
-					'<td><input class="player playerName" type="text" value="'+ pokerClock.players[i].name + '"></input></td>'+
-					'<td><input class="player text buyIn" type="text" value="'+ pokerClock.players[i].buyIn + '"></input></td>'+
-					'<td><input class="player text chips" type="text" value="'+ pokerClock.players[i].chips + '"></input></td>'+
-					'<td class="player cmd">' +
-						'<table><tr><td>' +
-						'<button title="knock out player"  class="koButton ui-state-default" >ko</button> ' +
-						'</td>'+
-						'<td><button title="rebuy" class="rebuy ui-state-default">rebuy</button> </td>'+
-						'<td><button title="rebuy" class="addon ui-state-default">add</button> </td>'+
-						'<td><button title="delete player"  class="delPlayer ui-state-default" >x</button> ' +
-						'</td></tr>'+
-					'</td>'+
-				'</tr>';
-			$("#players").append(rowString);
-			if(pokerClock.players[i].ko === true){
-				$("#players tbody tr.ko").find("input").addClass('ko');
-			}
-			totalBuyIn += parseInt(pokerClock.players[i].buyIn );
-			totalChips += parseInt(pokerClock.players[i].chips);
-		}
-		var activePlayers = $("tr.player").not("tr.ko").length;
-		$(".playersLeft").html(activePlayers);
-		//alert(activePlayers);
-		$(".avgStack").html(parseInt(totalChips / activePlayers));
-		$("#pot").html(totalBuyIn);
-		$("#totalChips").html(totalChips);
-		$("#players").trigger("update");
-		$(".poPercent").change();
-	},
-
-	updatePlayers :  function(){
-		pokerClock.players = [];
-		$("#players tr.player").each(function(){
-			var name = $(this).find(".playerName").val();
-			var buyIn = $(this).find(".buyIn").val();
-			var chips = $(this).find(".chips").val();
-			var seat = $(this).find(".seat").html();
-			var ko = false;
-			if($(this).hasClass('ko')){
-				ko = true;
-			}
-			var player = {'name': name, 'buyIn': buyIn, 'chips': chips, 'seat' : seat, 'ko' : ko};
-			pokerClock.players.push(player);
-		});
-		pokerClock.showPlayers();
-		$(this).select();
-	},
-	randomizeSeats : function(){
-		var seatIndex = pokerClock.players.length ;
-		var seats = [];
-		for (var i = 1; i <=seatIndex;i++){
-			seats.push(i);
-		}
-		for (var i in pokerClock.players){
-			pokerClock.players[i].seat = seats.sort(pokerClock.randomSort).pop();
-		}
-		pokerClock.showPlayers();
-	},
 	randomSort : function(a,b){
 		return( parseInt( Math.random()*10 ) %2 );
 	},
@@ -552,61 +458,14 @@ var pokerClock = {
 		}
 		$(".poPercent").change();
 	},
-	// addPayout : function(){
-	// 	if (pokerClock.payouts.length < pokerClock.players.length ){
-	// 		pokerClock.payouts.push(pokerClock.defaultPayout);
-	// 		pokerClock.showPayouts();
-	// 		$(".poPercent").change();
-	// 	}
-	// },
-	// delPayout : function(){
-	// 	if (pokerClock.payouts.length > 1){
-	// 		pokerClock.payouts.pop();
-	// 		pokerClock.showPayouts();
-	// 		$(".poPercent").change();
-	// 	}
-	// },
-	calculatePayoutDollars : function(){
-		var pot = parseFloat($("#pot").html());
-		var percent = $(this).attr('value') * .01;
-		var payout = (pot * percent);
-		$(this).parent().parent().find(".poDollars").val(payout);
-		var totalPercent = 0;
-		$(".poPercent").each(function(){
-			totalPercent += parseInt( $(this).val());
-		});
-		if(totalPercent == 100){
-			$("td.payout input").removeClass('over under').addClass('winner');
-		}else if(totalPercent > 100) {
-			$("td.payout input").removeClass('under winner').addClass('over');
-		}else if (totalPercent < 100){
-			$("td.payout input").removeClass('over winner').addClass('under');
-		}
-		pokerClock.payouts = [];
-		$("tr.payout").each(function(){
-			var percent = $(this).find("input.poPercent").val();
-			pokerClock.payouts .push({'percent':percent});
-		});
-	},
+
 	loadStructure : function(sIndex) {
 		pokerClock.rounds = pokerClock.structures[sIndex].rounds;
 		pokerClock.currentRound = 0;
 		pokerClock.startRound(pokerClock.currentRound);
-		//pokerClock.showPayouts();
 		pokerClock.showRounds();
 	},
 
-	loadPayStructure : function(sIndex) {
-		pokerClock.payouts = pokerClock.payStructures[sIndex].payouts;
-		 //	alert(sIndex);
-		//pokerClock.showPayouts();
-	},
-	showPayStructures : function(){
-		for(i in pokerClock.payStructures){
-			var optString = '<option value="'+i+'">' + pokerClock.payStructures[i].name + '</option>';
-			$("#payStructure").append(optString);
-		}
-	},
 	showStructures : function(){
 		for(i in pokerClock.structures){
 			var optString = '<option value="'+i+'">' + pokerClock.structures[i].structureName + '</option>';
@@ -814,93 +673,159 @@ var pokerClock = {
 			]
 		},
 		//end structure
-	],
-	payStructures :
-	[
+		//begin structure
 		{
-			name: 'Pay 2',
-			payouts: [
-				{percent:65, dollars: 0},
-				{percent:35, dollars: 0},
+			structureName : 'Humboldt $10 2000 chips',
+			rounds : [
+
+				{minutes: 15, small: 25, big: 50, ante: 0},
+				{minutes: 15, small: 50, big: 100, ante: 0},
+				{minutes: 15, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 400, big: 800, ante: 0},
+				{minutes: 15, small: 800, big: 1600, ante: 0},
+				{minutes: 15, small: 1500, big: 3000, ante: 0},
+				{minutes: 15, small: 3000, big: 6000, ante: 0},
+				{minutes: 15, small: 5000, big: 10000, ante: 0},
+
 			]
 		},
+		//end structure
+		//begin structure
 		{
-			name: 'Pay 3',
-			payouts: [
-				{percent:50, dollars: 0},
-				{percent:30, dollars: 0},
-				{percent:20, dollars: 0},
+			structureName : 'Humboldt $15 3000 chips - A',
+			rounds : [
+
+				{minutes: 20, small: 25, big: 50, ante: 0},
+				{minutes: 20, small: 50, big: 100, ante: 0},
+				{minutes: 20, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 400, big: 800, ante: 0},
+				{minutes: 15, small: 800, big: 1600, ante: 0},
+				{minutes: 15, small: 1500, big: 3000, ante: 0},
+				{minutes: 15, small: 3000, big: 6000, ante: 0},
+				{minutes: 15, small: 5000, big: 10000, ante: 0},
+				{minutes: 15, small: 10000, big: 20000, ante: 0},
+
 			]
 		},
+		//end structure
+		//begin structure
 		{
-			name: 'Pay 4',
-			payouts: [
-				{percent:45, dollars: 0},
-				{percent:25, dollars: 0},
-				{percent:18, dollars: 0},
-				{percent:12, dollars: 0},
+			structureName : 'Humboldt $15 4000 chips - B',
+			rounds : [
+
+				{minutes: 20, small: 25, big: 50, ante: 0},
+				{minutes: 20, small: 50, big: 100, ante: 0},
+				{minutes: 15, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 300, big: 600, ante: 100},
+				{minutes: 15, small: 500, big: 1000, ante: 100},
+				{minutes: 15, small: 1000, big: 2000, ante: 300},
+				{minutes: 15, small: 2000, big: 4000, ante: 500},
+				{minutes: 15, small: 3000, big: 6000, ante: 1000},
+				{minutes: 15, small: 5000, big: 10000, ante: 2000},
+				{minutes: 15, small: 10000, big: 20000, ante: 4000},
+
 			]
 		},
+		//end structure
+		//begin structure
 		{
-			name: 'Pay 5',
-			payouts: [
-				{percent:40, dollars: 0},
-				{percent:23, dollars: 0},
-				{percent:16, dollars: 0},
-				{percent:12, dollars: 0},
-				{percent:9, dollars: 0},
+			structureName : 'Humboldt $20 5000 chips - A',
+			rounds : [
+
+				{minutes: 20, small: 25, big: 50, ante: 0},
+				{minutes: 20, small: 50, big: 100, ante: 0},
+				{minutes: 20, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 300, big: 600, ante: 0},
+				{minutes: 15, small: 400, big: 800, ante: 0},
+				{minutes: 15, small: 600, big: 1200, ante: 0},
+				{minutes: 15, small: 800, big: 1600, ante: 0},
+				{minutes: 15, small: 1000, big: 2000, ante: 0},
+				{minutes: 15, small: 2000, big: 4000, ante: 0},
+				{minutes: 15, small: 3000, big: 60000, ante: 0},
+				{minutes: 15, small: 5000, big: 10000, ante: 0},
+				{minutes: 15, small: 8000, big: 16000, ante: 0},
+				{minutes: 15, small: 10000, big: 20000, ante: 0},
+
 			]
 		},
+		//end structure
+		//begin structure
 		{
-			name: 'Pay 6',
-			payouts: [
-				{percent:38, dollars: 0},
-				{percent:22, dollars: 0},
-				{percent:15, dollars: 0},
-				{percent:11, dollars: 0},
-				{percent:8, dollars: 0},
-				{percent:6, dollars: 0},
+			structureName : 'Humboldt $20 7500 chips - B',
+			rounds : [
+
+				{minutes: 20, small: 25, big: 50, ante: 0},
+				{minutes: 20, small: 50, big: 100, ante: 0},
+				{minutes: 20, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 300, big: 600, ante: 0},
+				{minutes: 15, small: 400, big: 800, ante: 0},
+				{minutes: 15, small: 600, big: 1200, ante: 100},
+				{minutes: 15, small: 800, big: 1600, ante: 200},
+				{minutes: 15, small: 1000, big: 2000, ante: 300},
+				{minutes: 15, small: 1500, big: 3000, ante: 500},
+				{minutes: 15, small: 2000, big: 4000, ante: 500},
+				{minutes: 15, small: 3000, big: 60000, ante: 1000},
+				{minutes: 15, small: 5000, big: 10000, ante: 1500},
+				{minutes: 15, small: 8000, big: 16000, ante: 2000},
+				{minutes: 15, small: 10000, big: 20000, ante: 3000},
+
 			]
 		},
+		//end structure
+		//begin structure
 		{
-			name: 'Pay 7',
-			payouts: [
-				{percent:35, dollars: 0},
-				{percent:21, dollars: 0},
-				{percent:15, dollars: 0},
-				{percent:11, dollars: 0},
-				{percent:8, dollars: 0},
-				{percent:6, dollars: 0},
-				{percent:4, dollars: 0},
+			structureName : 'Humboldt $20 / 4,500 chips - C',
+			rounds : [
+
+				{minutes: 20, small: 25, big: 50, ante: 0},
+				{minutes: 20, small: 50, big: 100, ante: 0},
+				{minutes: 20, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 300, big: 600, ante: 0},
+				{minutes: 15, small: 400, big: 800, ante: 0},
+				{minutes: 15, small: 600, big: 1200, ante: 100},
+				{minutes: 15, small: 800, big: 1600, ante: 200},
+				{minutes: 15, small: 1000, big: 2000, ante: 300},
+				{minutes: 15, small: 1500, big: 3000, ante: 500},
+				{minutes: 15, small: 2000, big: 4000, ante: 500},
+				{minutes: 15, small: 3000, big: 60000, ante: 1000},
+				{minutes: 15, small: 5000, big: 10000, ante: 1500},
+				{minutes: 15, small: 8000, big: 16000, ante: 2500},
+				{minutes: 15, small: 10000, big: 20000, ante: 3000},
+
 			]
 		},
+		//end structure
+		//begin structure
 		{
-			name: 'Pay 8',
-			payouts: [
-				{percent:33, dollars: 0},
-				{percent:20, dollars: 0},
-				{percent:15, dollars: 0},
-				{percent:11, dollars: 0},
-				{percent:8, dollars: 0},
-				{percent:6, dollars: 0},
-				{percent:4, dollars: 0},
-				{percent:3, dollars: 0},
+			structureName : 'Humboldt $25 / 10000 chips',
+			rounds : [
+
+				{minutes: 20, small: 25, big: 50, ante: 0},
+				{minutes: 20, small: 50, big: 100, ante: 0},
+				{minutes: 20, small: 100, big: 200, ante: 0},
+				{minutes: 15, small: 200, big: 400, ante: 0},
+				{minutes: 15, small: 300, big: 600, ante: 0},
+				{minutes: 15, small: 400, big: 800, ante: 0},
+				{minutes: 15, small: 600, big: 1200, ante: 100},
+				{minutes: 15, small: 800, big: 1600, ante: 200},
+				{minutes: 15, small: 1000, big: 2000, ante: 300},
+				{minutes: 15, small: 1500, big: 3000, ante: 500},
+				{minutes: 15, small: 2000, big: 4000, ante: 500},
+				{minutes: 15, small: 3000, big: 6000, ante: 1000},
+				{minutes: 15, small: 4000, big: 8000, ante: 1000},
+				{minutes: 15, small: 6000, big: 12000, ante: 2000},
+				{minutes: 15, small: 8000, big: 1600, ante: 3000},
+				{minutes: 15, small: 10000, big: 20000, ante: 4000},
+
 			]
 		},
-		{
-			name: 'Pay 9',
-			payouts: [
-				{percent:32, dollars: 0},
-				{percent:20, dollars: 0},
-				{percent:14, dollars: 0},
-				{percent:11, dollars: 0},
-				{percent:8, dollars: 0},
-				{percent:6, dollars: 0},
-				{percent:4, dollars: 0},
-				{percent:3, dollars: 0},
-				{percent:2, dollars: 0},
-			]
-		}
+		//end structure
 	]
 
 };
